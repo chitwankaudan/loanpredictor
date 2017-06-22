0c4dfb5ea8aac72c2101e76f86fb880cb48c0fb0
chitwankaudan <chitwank@berkeley.edu>
2017-06-22 12:07:14 -0700

    Update assemble.py

--- assemble.py
+++ assemble.py
@@ -3,6 +3,7 @@ import settings
 import datetime
 import pandas as pd
 
+# list all revelant columns
 SELECT = [
 "id",
 "loan_amnt",
@@ -31,14 +32,24 @@ SELECT = [
 ]
 
 def concatenate():
-  file = os.listdir(settings.DATA_DIR)
+  files = os.listdir(settings.DATA_DIR)
   alldata = []
+  
+  # keep the header of the first file
+  first = pd.read_csv(os.path.join(settings.DARA_DIR, files[0]), header = 1, index_col=False)
+  first = first[SELECTED]
+  alldata.append(first)
+  
+  # drop the header in the rest of the files
+  files = files[1:]
   for f in files:
     data = pd.read_csv(os.path.join(settings.DATA_DIR, f), header = 1, index_col=False)
-    data = data[SELECT]
+    data = data[SELECT] # trim dataframe to only included "selected" columns
+    data.drop(0, inplace = True)  # drop header
     alldata.append(data)
     
-  alldata = pd.concat(all_data, axis=0)
-  alldata.to_csv(os.path.join(PROCESSED_DIR, "alldata.text"), sep = ",", header = SELECT, index = False)
-  
+  alldata = pd.concat(all_data, axis=0) #merge all dataframes
+  alldata.to_csv(os.path.join(PROCESSED_DIR, "alldata.text"), sep = ",", header = SELECT, index = False)  #write data to alldata.txt
+ 
+
  
