24c827c177d15063bee109aa452c8ecf086e4fe8
Chitwan Kaudan <chitwank@berkeley.edu>
2017-07-05 23:47:01 -0700

    Near final version of assemble file

--- assemble.py
+++ assemble.py
@@ -2,19 +2,17 @@ import os
 import settings
 import datetime
 import pandas as pd
+import numpy as np
 
 # lists all revelant columns
 SELECT = [
 "id",
 "loan_amnt",
-"funded_amnt",
-"funded_amnt_inv",
 "term",
 "int_rate",
 "installment",
 "grade",
 "sub_grade",
-"emp_title",
 "emp_length",
 "home_ownership",
 "annual_inc",
@@ -31,13 +29,39 @@ SELECT = [
 "application_type"
 ]
 
+type_dict = {
+"id": "object",
+"loan_amnt": "object",
+"term": "object",
+"int_rate": "object",
+"installment": "float64",
+"grade": "object",
+"sub_grade": "object",
+"emp_title": "object",
+"emp_length": "object",
+"home_ownership": "object",
+"annual_inc": "float64",
+"verification_status": "object",
+"issue_d": "object",
+"purpose": "object",
+"addr_state": "object",
+"dti": "float64",
+"earliest_cr_line": "object",
+"open_acc": "float64",
+"pub_rec": "float64",
+"total_acc": "float64",
+"initial_list_status": "object",
+"application_type": "object"
+}
+
+
 def concatenate():
   files = os.listdir(settings.DATA_DIR)
   alldata = []
   
   # keeps the header of the first file
-  first = pd.read_csv(os.path.join(settings.DARA_DIR, files[0]), header = 1, index_col=False)
-  first = first[SELECTED]
+  first = pd.read_csv(os.path.join(settings.DATA_DIR, files[0]), header = 1, index_col=False, dtype = type_dict, na_values = [""])
+  first = first[SELECT]
   alldata.append(first)
   
   # drops the header in the rest of the files
@@ -49,13 +73,13 @@ def concatenate():
     alldata.append(data)
   
   # concatenates all data into one file
-  alldata = pd.concat(all_data, axis=0) # merges all dataframes
-  alldata[id] = range(1, alldata.shape[0])  # adds a unique id
-  alldata.to_csv(os.path.join(PROCESSED_DIR, "alldata.text"), sep = ",", header = SELECT, index = False)  #write data to alldata.txt
+  alldata = pd.concat(alldata, axis=0) # merges all dataframes
+  # alldata[id] = range(1, (alldata.shape[0]-5)  # adds a unique id
+  alldata.to_csv(os.path.join(settings.PROCESSED_DIR, "alldata.txt"), sep = ",", header = SELECT, index = False)  #write data to alldata.txt
              
     
 # runs concatenate only if assemble.py called from the command line
-if _name_ == "_main_" :
+if __name__ == "__main__" :
   concatenate()
   
   
